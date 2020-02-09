960ccdfdd56b5db9293486c803e831b4b51035d2
LouizEchi <louizechiverri@gmail.com>
2020-02-09 22:38:23 +0800

    feat: 🎸 Initial Commit

---
+++ packages/users/handler.ts
@@ -0,0 +1,20 @@
+import { APIGatewayEvent, Context, Handler, Callback } from 'aws-lambda';
+
+export const graphqlHandler: Handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  cb: Callback
+) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  cb(null, response);
+};
